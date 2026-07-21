052b6ef436335a5079cba1eac6a39c94633740dd
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-07-21 14:51:22 +0400

    Check tables after creating them and adding data into

--- JoinPracticeDB-Task4.sql
+++ JoinPracticeDB-Task4.sql
@@ -10,3 +10,28 @@ CREATE TABLE Sales (
     SaleID INT PRIMARY KEY,
     ProductID INT
 );
+
+-- Add data into Products table
+
+INSERT INTO Products (ProductID, ProductName)
+VALUES
+(1, 'Laptop'),
+(2, 'Mouse'),
+(3, 'Keyboard'),
+(4, 'Monitor'),
+(5, 'Printer');
+
+-- Add data into Sales table
+-- ProductID 6 does not exist in Products to test FULL OUTER JOIN
+
+INSERT INTO Sales (SaleID, ProductID)
+VALUES
+(301, 1),
+(302, 2),
+(303, 2),
+(304, 6);
+
+-- check tables after create
+SELECT * FROM Products;
+
+SELECT * FROM Sales;
