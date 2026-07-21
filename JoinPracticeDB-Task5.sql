68257c4047b9f4183252edb602699fa6c55eaed9
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-07-21 15:23:34 +0400

    add data into EmployeeManagers table

--- JoinPracticeDB-Task5.sql
+++ JoinPracticeDB-Task5.sql
@@ -6,3 +6,15 @@ CREATE TABLE EmployeeManagers (
     EmployeeName VARCHAR(50),
     ManagerID INT
 );
+-- Add data into EmployeeManagers table
+-- ManagerID refers to another EmployeeID in the same table
+
+INSERT INTO EmployeeManagers (EmployeeID, EmployeeName, ManagerID)
+VALUES
+(1, 'Ahmed Al Balushi', NULL),
+(2, 'Sara Al Hinai', 1),
+(3, 'Mohammed Al Riyami', 1),
+(4, 'Fatma Al Saadi', 2),
+(5, 'Ali Al Mukhaini', 2),
+(6, 'Mariam Al Zadjali', 3),
+(7, 'Khalid Al Abri', 3);
