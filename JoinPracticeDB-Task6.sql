d0be351ba896813a4b9f4919f0f2af503e339d7f
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-07-21 15:55:28 +0400

    Add data into Sizes table, Colors Table

--- JoinPracticeDB-Task6.sql
+++ JoinPracticeDB-Task6.sql
@@ -11,3 +11,19 @@ CREATE TABLE Sizes (
     SizeName VARCHAR(50)
 );
 
+-- Add data into Colors table
+INSERT INTO Colors (ColorID, ColorName)
+VALUES
+(1, 'Black'),
+(2, 'White'),
+(3, 'Red'),
+(4, 'Blue'),
+(5, 'Green');
+
+-- Add data into Sizes table
+INSERT INTO Sizes (SizeID, SizeName)
+VALUES
+(1, 'Small'),
+(2, 'Medium'),
+(3, 'Large'),
+(4, 'Extra Large');
