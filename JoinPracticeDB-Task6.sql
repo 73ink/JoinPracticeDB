01643b035b251e6a742e1b1c5b119ff432d79518
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-07-21 15:54:55 +0400

    create tables of colors and sizes

---
+++ JoinPracticeDB-Task6.sql
@@ -0,0 +1,13 @@
+
+-- Create Colors table
+CREATE TABLE Colors (
+    ColorID INT PRIMARY KEY,
+    ColorName VARCHAR(50)
+);
+
+-- Create Sizes table
+CREATE TABLE Sizes (
+    SizeID INT PRIMARY KEY,
+    SizeName VARCHAR(50)
+);
+
