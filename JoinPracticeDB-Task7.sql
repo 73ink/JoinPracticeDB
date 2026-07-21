320781155f3d4ad86dcfe58b0b1156e569f54935
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-07-21 16:08:49 +0400

    Task 7: INNER JOIN with Multiple Tables

---
+++ JoinPracticeDB-Task7.sql
@@ -0,0 +1,11 @@
+
+-- Task 7: INNER JOIN with Multiple Tables
+
+-- Tables: Students, Classes, Teachers (already created with task 1, onle create Teachers)
+
+
+-- Create Teachers table
+CREATE TABLE Teachers (
+    TeacherID INT PRIMARY KEY,
+    TeacherName VARCHAR(50)
+);
