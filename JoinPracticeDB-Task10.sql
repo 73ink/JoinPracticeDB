9f7841383c098021083bdfc8ddeb308885350be4
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-07-22 09:29:09 +0400

    Add data into inventory table and sales table

--- JoinPracticeDB-Task10.sql
+++ JoinPracticeDB-Task10.sql
@@ -19,3 +19,20 @@ CREATE TABLE Sales (
     SoldQty INT
 );
 
+-- Add data into Inventory table
+INSERT INTO Inventory (ProductID, ProductName, StockQty)
+VALUES
+(1, 'Laptop', 20),
+(2, 'Mouse', 100),
+(3, 'Keyboard', 60),
+(4, 'Monitor', 35),
+(5, 'Printer', 15);
+
+-- Add data into Sales table
+-- ProductID 6 does not exist in Inventory to test FULL OUTER JOIN
+INSERT INTO Sales (ProductID, SoldQty)
+VALUES
+(1, 5),
+(2, 25),
+(3, 10),
+(6, 8);
