aae0aa6cf3fb1a176c3aea75141d49c78e3f167c
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-07-22 09:33:46 +0400

    Task 12: Create tables: Stores, StoreProducts

---
+++ JoinPracticeDB-Task12.sql
@@ -0,0 +1,13 @@
+-- Task 12: CROSS JOIN with Business Scenario
+
+
+-- Create StoreProducts table
+CREATE TABLE StoreProducts (
+    ProductName VARCHAR(50)
+);
+
+-- Create Stores table
+CREATE TABLE Stores (
+    StoreName VARCHAR(50)
+);
+
