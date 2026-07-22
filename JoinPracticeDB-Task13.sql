9ddca12dbf96e9a2cf127a731c47677caf4de587
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-07-22 09:39:43 +0400

    Create OrderDetails table

---
+++ JoinPracticeDB-Task13.sql
@@ -0,0 +1,12 @@
+-- Task 13: INNER JOIN with Aggregation
+
+
+-- Create OrderDetails table
+CREATE TABLE OrderDetails (
+    OrderDetailID INT PRIMARY KEY,
+    OrderID INT,
+    ProductID INT,
+    Quantity INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
