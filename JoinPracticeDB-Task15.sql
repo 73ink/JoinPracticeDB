c0aa5942f9a7ab34ffc67e80bc3a8e1f5423ffb9
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-07-22 09:47:41 +0400

    Add data into Courses table, and Enrollments table: some students have enrolment into a course, and some aren't

--- JoinPracticeDB-Task15.sql
+++ JoinPracticeDB-Task15.sql
@@ -14,3 +14,28 @@ CREATE TABLE Enrollments (
     FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
     FOREIGN KEY (CourseID) REFERENCES Courses(CourseID)
 );
+
+-- Add data into Courses table
+INSERT INTO Courses (CourseID, CourseName)
+VALUES
+(1, 'SQL Basics'),
+(2, 'Java Fundamentals'),
+(3, 'HTML and CSS'),
+(4, 'Spring Boot'),
+(5, 'Cybersecurity Basics');
+
+-- Add data into Enrollments table
+-- Some students are enrolled in courses
+-- Some students will have no enrollment to test LEFT JOIN
+INSERT INTO Enrollments (StudentID, CourseID)
+VALUES
+(112, 1),
+(113, 2),
+(114, 3),
+(115, 4),
+(116, 1),
+(117, 5),
+(118, 2),
+(211, 3),
+(212, 4);
+
