50ad1017c80710dabbf1fa856c90ebdf02272e92
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-07-22 09:41:14 +0400

    Check tables with SELECT

--- JoinPracticeDB-Task13.sql
+++ JoinPracticeDB-Task13.sql
@@ -10,3 +10,19 @@ CREATE TABLE OrderDetails (
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
 
+-- Add data into OrderDetails table
+INSERT INTO OrderDetails (OrderDetailID, OrderID, ProductID, Quantity)
+VALUES
+(1, 201, 1, 2),
+(2, 201, 2, 3),
+(3, 202, 3, 1),
+(4, 202, 4, 4),
+(5, 203, 1, 1),
+(6, 203, 5, 2),
+(7, 204, 2, 5);
+
+-- Check Orders table
+SELECT * FROM Orders;
+
+-- Check OrderDetails table
+SELECT * FROM OrderDetails;
