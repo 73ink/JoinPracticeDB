5f17b04a838a0b2bd784890316727b7372bef8d4
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-07-22 09:44:19 +0400

    Task 14: LEFT JOIN with COUNT, This includes customers with zero orders

--- JoinPracticeDB-Task14.sql
+++ JoinPracticeDB-Task14.sql
@@ -7,3 +7,14 @@ SELECT * FROM Customers;
 -- Check Orders table
 SELECT * FROM Orders;
 
+-- LEFT JOIN with COUNT
+-- This includes customers with zero orders
+SELECT 
+    Customers.CustomerID,
+    Customers.CustomerName AS Name,
+    COUNT(Orders.OrderID) AS OrderCount
+FROM Customers
+LEFT JOIN Orders
+ON Customers.CustomerID = Orders.CustomerID
+GROUP BY Customers.CustomerID, Customers.CustomerName
+ORDER BY OrderCount DESC;
